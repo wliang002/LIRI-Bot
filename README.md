f323589180746da68dc71fada2bf9e86b6adfafc
wliang002 <wliang002@berkeley.edu>
2019-05-09 16:04:58 -0700

    fixed

--- README.md
+++ README.md
@@ -1,3 +1,77 @@
-# LIRI-Bot
-* LIRI is a Language Interpretation and Recognition Interface. 
-* LIRI will be a command line node app that takes in parameters and gives you back data.
+# LIRI-Bot 1.0
+
+## Overview
+	* LIRI is a Language Interpretation and Recognition Interface. 
+	* LIRI will be a command line node app that takes in parameters and gives you back data.
+
+## Packages download
+* Dotenv 
+```bash
+npm install dotenv
+```
+* Moment
+```bash
+npm install moment
+```
+* Axios
+```bash
+npm install axios
+```
+* Spotify
+```bash
+npm install --save node-spotify-api
+```
+
+
+## LIRI Commands
+* concert-this
+* spotify-this-song
+* movie-this
+* do-what-it-says
+
+## Demo
+* Runing Liri.js without any command
+```bash
+node liri.js 
+```
+![liri](images/liri)
+
+* Find concerts
+```bash
+node liri.js concert-this lady gaga
+```
+![concert-gaga](images/concert-ladygaga)
+
+```bash
+node liri.js concert-this adam levine
+```
+![concert-adam](images/concert-adam)
+
+* Find songs
+```bash
+node liri.js spotify-this-song yesterday
+```
+![spotify-yesterday](images/spotify-yesterday)
+
+```bash
+node liri.js spotify-this-song
+```
+![spotify](images/spotify)
+
+* Find movies
+```bash
+node liri.js movie-this avengers
+```
+![movie-avengers](images/movie-this-avengers)
+
+```bash
+node liri.js movie-this 
+```
+![movie](images/movie-this)
+
+* Read Files
+```bash
+node liri.js do-what-it-says
+```
+![do-what-it-says](images/do-what-it-says)
+
--- liri.js
+++ liri.js
@@ -3,47 +3,51 @@ var keys = require("./key.js");
 
 var userCommand = process.argv[2];
 var userInput = process.argv;
-userInput = userInput.slice(3, userInput.length+1);
-var valid;
+userInput = userInput.slice(3, userInput.length + 1);
+userInput = userInput.join(" ");
 var moment = require('moment');
 var keys = require('./key');
 var Spotify = require('node-spotify-api');
 var axios = require('axios');
 
-runLiri(userCommand, userInput);
+
+if (userCommand === "do-what-it-says") {
+    console.log(userCommand)
+    readFile()
+} else {
+    runLiri(userCommand, userInput);
+}
+
 
 
 function runLiri(command, input) {
-if (command !== undefined) {
-    switch(userCommand) {
-        case "concert-this":
-            getConcert(input)
-        break;
-        case "spotify-this-song":
-            getMusic(input)
-        break;
-        case "movie-this":
-            getMovie(input)
-        break;
-        case "do-what-it-says":
-            readFile()
-        default:
-            console.log("sorry I don't know that command")
-    }
-} else {
-    return console.log(`Input a liri command:
+    if (command !== undefined) {
+        switch (command) {
+            case "concert-this":
+                getConcert(input)
+                break;
+            case "spotify-this-song":
+                getMusic(input)
+                break;
+            case "movie-this":
+                getMovie(input)
+                break;
+            default:
+                console.log("sorry I don't know that command")
+        }
+    } else {
+        return console.log(`Input a liri command:
                         concert-this
                         spotify-this-song
                         movie-this
                         do-what-it-says`)
-}
+    }
 }
 
 // Bands in town
-function getConcert() {
-    validateuserInput("concert-this");
-    if(valid) {        
-        var queryParam =userInput.join("%20");
+function getConcert(input) {
+    if (input.length > 0) {
+        var queryParam = input;
         queryParam = queryParam.replace(/\//g, "%252F");
         queryParam = queryParam.replace(/\?/g, "%253F");
         queryParam = queryParam.replace(/\*/g, "%252A");
@@ -52,114 +56,131 @@ function getConcert() {
         var queryURL = `${baseURL}${endpoint}?app_id=${keys.bands.app_id}`
 
         axios.get(queryURL)
-        .then(concerts => {
-            var concertResults = concerts.data;
-            if(typeof(concertResults) != "object") {
-                console.log("Can't find artist. Try again.")
-            }else if(concertResults.length >0) {
-                concertResults.forEach(concert => {
-                    var date = moment(concert.datetime).format("MM/DD/YYYY")
-                    var time = moment(concert.datetime).format("hh:mm A")
-                    console.log("\n*************");
-                    console.log(userInput.join(" "))
-                    console.log(`Venue: ${concert.venue.name}`)
-                    console.log(`Location: ${concert.venue.city}, ${concert.venue.region},${concert.venue.country}`)
-                    console.log(`Date: ${date}`)
-                    console.log(`Time: ${time}`)
-                   
-                })
-            } else {
-                console.log(`There aren't any upcoming events for ${userInput.join(" ")}`)
-            }
-            
-           
-        })
-        .catch(err => console.log(err));
+            .then(concerts => {
+                var concertResults = concerts.data;
+                if (typeof (concertResults) != "object") {
+                    console.log("Can't find artist. Try again.")
+                } else if (concertResults.length > 0) {
+                    concertResults.forEach(concert => {
+                        var date = moment(concert.datetime).format("MM/DD/YYYY")
+                        var time = moment(concert.datetime).format("hh:mm A")
+                        console.log("\n*************");
+                        console.log(queryParam)
+                        console.log(`Venue: ${concert.venue.name}`)
+                        console.log(`Location: ${concert.venue.city}, ${concert.venue.region},${concert.venue.country}`)
+                        console.log(`Date: ${date}`)
+                        console.log(`Time: ${time}`)
 
-    }  
+                    })
+                } else {
+                    console.log(`There aren't any upcoming events for ${queryParam}`)
+                }
+
+
+            })
+            .catch(err => console.log(err));
+
+    } else {
+        return console.log("******\n" + "You need to input a search term\n******")
+    }
 
 }
 
 // Spotify
-function getMusic() {
-    validateuserInput("spotity-this-song");
-  if(valid) {
-      var spotify  = new Spotify({
-        id: keys.spotify.id,
-        secret: keys.spotify.secret
-      });
+function getMusic(input) {
+    if (input.length > 0) {
+        var spotify = new Spotify({
+            id: keys.spotify.id,
+            secret: keys.spotify.secret
+        });
 
-      var queryParam = userInput.join(" ");
-    
-      spotify.search({ type: 'track', query: queryParam})
-      .then(function(data) {   
-        var searchResult = data.tracks.items
-        if (searchResult.length > 0) {
-           searchResult.forEach(song => {
-              console.log(`****************************`);
-              console.log(`Artist: ${song.artists[0].name}`);
-              console.log(`Song Name: ${song.name}`);
-              console.log(`Preview Link: ${song.preview_url}`);
-              console.log(`Album: ${song.album.name}`);
-              console.log(`****************************`);
-           });
-        }
-    }).catch(function(err) {
-               console.log(err)
-    });   
-      
-  }
+        var queryParam = input;
+
+        spotify.search({ type: 'track', query: queryParam })
+            .then(function (data) {
+                var searchResult = data.tracks.items
+                if (searchResult.length > 0) {
+                    searchResult.forEach(song => {
+                        console.log(`****************************`);
+                        console.log(`Artist: ${song.artists[0].name}`);
+                        console.log(`Song Name: ${song.name}`);
+                        console.log(`Preview Link: ${song.preview_url}`);
+                        console.log(`Album: ${song.album.name}`);
+                        console.log(`****************************`);
+                    });
+                }
+            }).catch(function (err) {
+                console.log(err)
+            });
+
+    } else {
+        // getMusic("The Sign");
+        var spotify = new Spotify({
+            id: keys.spotify.id,
+            secret: keys.spotify.secret
+        });
+
+        var queryParam = "the sign";
+
+        spotify.search({ type: 'track', query: queryParam })
+            .then(function (data) {
+                var searchResult = data.tracks.items
+                var song = searchResult[7];
+                console.log(`****************************`);
+                console.log(`Artist: ${song.artists[0].name}`);
+                console.log(`Song Name: ${song.name}`);
+                console.log(`Preview Link: ${song.preview_url}`);
+                console.log(`Album: ${song.album.name}`);
+                console.log(`****************************`);
+            });
+    }
+
 }
 
-function getMovie() {
-    var baseURL = `http://www.omdbapi.com/?`
-    var moiveTitle = userInput.join("20%");
-    var queryURL = `${baseURL}t=${moiveTitle}&apikey=${keys.omdb.apikey}`
+function getMovie(input) {
+    if (input.length > 0) {
+        var baseURL = `http://www.omdbapi.com/?`
+        var moiveTitle = input;
+        var queryURL = `${baseURL}t=${moiveTitle}&apikey=${keys.omdb.apikey}`
 
-    axios.get(queryURL)
-    .then(movies => {
-        var movieResults = movies.data;
-        console.log(`*****************`);
-        console.log(`Movie Title: ${movieResults.Title}`);
-        console.log(`Year: ${movieResults.Year}`);
-        console.log(`IMDB Rating: ${movieResults.Ratings[0].Value}`);
-        console.log(`Rotten Tomatoes Rating: ${movieResults.Ratings[1].Value}`);
-        console.log(`Country: ${movieResults.Country}`);
-        console.log(`Language: ${movieResults.Language}`);
-        console.log(`Plot: ${movieResults.Plot}`);
-        console.log(`Actors: ${movieResults.Actors}`);
-        console.log(`*****************`);
-    }); 
+        axios.get(queryURL)
+            .then(movies => {
+                var movieResults = movies.data;
+                console.log(`*****************`);
+                console.log(`Movie Title: ${movieResults.Title}`);
+                console.log(`Year: ${movieResults.Year}`);
+                console.log(`IMDB Rating: ${movieResults.Ratings[0].Value}`);
+                console.log(`Rotten Tomatoes Rating: ${movieResults.Ratings[1].Value}`);
+                console.log(`Country: ${movieResults.Country}`);
+                console.log(`Language: ${movieResults.Language}`);
+                console.log(`Plot: ${movieResults.Plot}`);
+                console.log(`Actors: ${movieResults.Actors}`);
+                console.log(`*****************`);
+
+            }).catch(function (err) {
+                console.log(err)
+            });
+
+    } else {
+        getMovie("Mr. Nobody");
+    }
 
 
+}
 
-
-}  
-   
 
 
 function readFile() {
     var fs = require('fs');
-    fs.readFile('random.txt', 'utf8', function(err, data){
-        if(err) {
+    fs.readFile('random.txt', 'utf8', function (err, data) {
+        if (err) {
             console.log(err);
         }
         var textArray = data.split(',');
         var comm = textArray[0];
         var inp = textArray[1];
+        console.log(comm, inp)
+        runLiri(comm, inp);
     })
-
-    
-
-
 }
 
-// check if user typed any input after command
-function validateuserInput(command){
-    if(userInput.length > 0) {
-        valid = true;
-    } else {
-        valid = false;
-        return console.log("******\n" + command, "requires you to input a search term\n******")
-    }
-}
